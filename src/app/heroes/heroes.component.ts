4c33b5d1815ce6c11ceee31153ad1aac9597c8cb
deana <dvandegriff1@gmail.com>
2022-06-30 11:29:08 -0500

    adds and displays messages to hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,7 +13,7 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
  
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
@@ -25,6 +26,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
+
+  
 
 }
